d23c27a8bd7114b01b56ba948ab2080788d28a4e
Abi Gicic <gicic.abdulah@gmail.com>
2019-07-14 01:33:31 +0200

    Add dataset

---
+++ posters/dataset/downloader.py
@@ -0,0 +1,9 @@
+import os
+import requests
+
+def get_image(link):
+    response = requests.get(link)
+    print('got em')
+
+link = "https://images-na.ssl-images-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@._V1_UX182_CR0,0,182,268_AL_.jpg"
+get_image(link)
