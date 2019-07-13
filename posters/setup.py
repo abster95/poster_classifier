049579c307b831e55aed68022b09f29a2c04956c
Abi Gicic <gicic.abdulah@gmail.com>
2019-07-14 01:22:40 +0200

    Add package

---
+++ posters/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+import os
+
+
+setup(
+    name='posters',
+    version='19.07.14',
+    author='Abi Gicic',
+    author_email='gicic.abdulah@microsoft.com',
+    description='Posters classifier.',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+    ]
+)
