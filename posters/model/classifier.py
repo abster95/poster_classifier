97b0b7c08e8eaddb1a2b99f54315993f9ea7e7bc
Abi Gicic <gicic.abdulah@gmail.com>
2019-09-08 16:50:03 +0200

    Upgrade the classifier

--- posters/model/classifier.py
+++ posters/model/classifier.py
@@ -2,15 +2,22 @@ import torch
 import torch.nn as nn
 import torchvision
 
-from torchvision.models import resnet18
+from torchvision.models import resnet101
 
 
 class Classifier(nn.Module):
-    def __init__(self, backbone=resnet18, imagenet_weights=True, num_classes=20):
+    def __init__(self, backbone=resnet101, imagenet_weights=True, num_classes=20):
         super(Classifier, self).__init__()
         self.backbone = backbone(pretrained=imagenet_weights)
-        self.fc = nn.Linear(1000, num_classes)
+        self.fc1 = nn.Linear(1000, 512)
+        self.fc2 = nn.Linear(512, 128)
+        self.fc3 = nn.Linear(128, 32)
+        self.fc4 = nn.Linear(32, num_classes)
 
     def forward(self, img_batch):
         x = self.backbone(img_batch)
-        return self.fc(x)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+        return self.fc4(x)
+
