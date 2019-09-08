1fe2ad5d8ec785e1ac27d964af5bca3fffbe377d
Abi Gicic <gicic.abdulah@gmail.com>
2019-09-08 16:54:49 +0200

    Add fp and fn to accuracy

--- posters/training.py
+++ posters/training.py
@@ -27,15 +27,19 @@ class AverageMeter(object):
         self.count += n
         self.avg = self.sum / self.count
 
-def accuracy(output, target, thresh=0.5):
+def accuracy(output, target, thresh=0.7):
     """Computes the precision@k for the specified values of k"""
     correct = 0.0
+    fp = 0.0
+    fn = 0.0
     total = 0.0
     with torch.no_grad():
         thresholded = (output > thresh).float()
         total += target.size(0)
         correct += (thresholded==target).sum().item()
-    return correct/total
+        fp += (thresholded==(1-target)).sum().item()
+        fn += ((1-thresholded)==target).sum.item()
+    return correct/total, fp/total, fn/total
 
 def train(train_loader, model, criterion, optimizer, epoch):
     batch_time = AverageMeter()
@@ -69,15 +73,18 @@ def train(train_loader, model, criterion, optimizer, epoch):
         batch_time.update(time.time() - end)
         end = time.time()
 
-        accurate = accuracy(output, target_var)
+        accurate, fp, fn = accuracy(output, target_var)
 
         print('Epoch: [{0}][{1}/{2}]\t'
                 'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
                 'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
                 'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                'Accuracy {acc:.4f}'.format(
+                'Accuracy {acc:.4f}'
+                'FP {fp:.4f}\t'
+                'FN {fn:.4f}'.format(
                 epoch, i, len(train_loader), batch_time=batch_time,
-                data_time=data_time, loss=losses, acc=accurate))
+                data_time=data_time, loss=losses, acc=accurate,
+                fp=fp, fn=fn))
 
 def validate(val_loader, model, criterion):
     batch_time = AverageMeter()
@@ -101,12 +108,16 @@ def validate(val_loader, model, criterion):
         batch_time.update(time.time() - end)
         end = time.time()
 
-        accurate = accuracy(output, target_var)
+        accurate, fp, fn = accuracy(output, target_var)
         print('Test: [{0}/{1}]\t'
                 'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
                 'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                'Accuracy {acc:.4f}'.format(
-                i, len(val_loader), batch_time=batch_time, loss=losses, acc=accurate))
+                'Accuracy {acc:.4f}\t'
+                'FP {fp:.4f}\t'
+                'FN {fn:.4f}'.format(
+                i, len(val_loader), batch_time=batch_time,
+                loss=losses, acc=accurate,
+                fp=fp, fn=fn))
 
 if __name__ == "__main__":
 
